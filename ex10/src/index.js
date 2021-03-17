9b47913ed6ee5eb5a15909b616fb389999a9c2c8
sara-gheith <saraannegheith@gmail.com>
2021-03-17 21:24:20 +0100

    .hasOwnProperty

---
+++ ex10/src/index.js
@@ -0,0 +1,15 @@
+function myFunction() {
+    var myMusic = {
+        001: {
+            artist: "Billy Joel",
+            title: "Piano Man",
+            release_year: 1973,
+            formats: {
+                1: "CD",
+                2: "8T",
+                3: "LP"
+            },
+            gold: true
+        }, 
+    }
+}
