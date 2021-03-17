214dada8d988daa34374035a6b26e71cab7af3ed
sara-gheith <saraannegheith@gmail.com>
2021-03-17 15:12:15 +0100

    Adding Objects

---
+++ ex01/src/index.js
@@ -0,0 +1,16 @@
+// Only change code below this line
+
+var myPet = {
+  species: "Human",
+  name: "Sara",
+  legs: 2,
+  friends: ["Amina", "Dzeni"]
+};
+
+function myFunction(myObj) {
+  return myObj;
+}
+
+// Only change code above this line
+console.log(myFunction(myPet));
+module.exports = { myPet, myFunction };
